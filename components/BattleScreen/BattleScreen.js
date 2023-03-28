cd8573951d0125c19543bcbb5a171687480f2bfe
Dennis Buchwald <buchwald95@icloud.com>
2023-03-28 10:16:28 +0200

    finish player statebar

--- components/BattleScreen/BattleScreen.js
+++ components/BattleScreen/BattleScreen.js
@@ -1,7 +1,6 @@
 import styles from "./battlescreen.module.css";
 import PlayerState from "./PlayerState/PlayerState";
-import Mewtwo from "./Pokemon/EnemyPokemon";
-import Glurak from "./Pokemon/PlayerPokemon";
+
 import PlayerPokemon from "./Pokemon/PlayerPokemon";
 import EnemyPokemon from "./Pokemon/EnemyPokemon";
 
--- components/BattleScreen/PlayerState/PlayerState.js
+++ components/BattleScreen/PlayerState/PlayerState.js
@@ -1,21 +1,22 @@
 import styles from "./playerstate.module.css";
 
 export default function PlayerState() {
-  return (
-    <div className={styles["player--state-box"]}>
-      <div className={styles["player--state-wrapper"]}>
-        <p className={styles["player--pokemon-name"]}>Glurak</p>
-        <p className={styles["player--pokemon-level"]}>51</p>
-        <p className={styles["player--pokemon-health"]}>Player health</p>
-        <p
-          class="player--pokemon-health-bar"
-          role="progressbar"
-          aria-valuenow="80"
-          aria-valuemin="0"
-          aria-valuemax="80"
-        ></p>
-        <p class="player--pokemon-max-health">120</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles["player--state-box"]}>
+			<div className={styles["player--state-wrapper"]}>
+				<p className={styles["player--pokemon-name"]}>Glurak</p>
+				<p className={styles["player--pokemon-level"]}>52</p>
+				<p className={styles["player--pokemon-health"]}>120</p>
+				<p
+					class="player--pokemon-health-bar"
+					role="progressbar"
+					aria-valuenow="80"
+					aria-valuemin="0"
+					aria-valuemax="80"
+				></p>
+				<p className={styles["player--pokemon-slash-health"]}>/</p>
+				<p class={styles["player--pokemon-max-health"]}>120</p>
+			</div>
+		</div>
+	);
 }
