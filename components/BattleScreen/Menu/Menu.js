46da6764179f6aab7848b296eef6c059b6b12b0d
Dennis Buchwald <buchwald95@icloud.com>
2023-03-28 14:04:05 +0200

    fix statusbar

--- components/BattleScreen/Menu/Menu.js
+++ components/BattleScreen/Menu/Menu.js
@@ -5,15 +5,15 @@ export default function Menu() {
 		<div className={styles.menu}>
 			<div className={styles["menu--overview-box"]}>
 				<div className={styles["menu--overview-box-left"]}>
-					<p>Klicke auf Kampf um anzugreifen</p>
+					{/* <p>Klicke auf Kampf um anzugreifen</p> */}
 				</div>
 				<div className={styles["menu--overview-box-right"]}>
-					<div className={`${styles.btn} ${styles["btn--fight"]}`}>Kampf</div>
+					{/* <div className={`${styles.btn} ${styles["btn--fight"]}`}>Kampf</div>
 					<div className={`${styles.btn} ${styles["btn--bag"]}`}>Beutel</div>
 					<div className={`${styles.btn} ${styles["btn--pokemon"]}`}>
 						POKéMON
 					</div>
-					<div className={`${styles.btn} ${styles["btn--run"]}`}>Flucht</div>
+					<div className={`${styles.btn} ${styles["btn--run"]}`}>Flucht</div> */}
 				</div>
 			</div>
 		</div>
