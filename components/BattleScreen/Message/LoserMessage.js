a10cbc5ee1e126458261a84403bae59c17d2632f
Dennis Buchwald <buchwald95@icloud.com>
2023-04-05 10:24:34 +0200

    rework message screen

--- components/BattleScreen/Message/LoserMessage.js
+++ components/BattleScreen/Message/LoserMessage.js
@@ -1,19 +1,23 @@
 import styled from "styled-components";
 
 export default function LoserMessage() {
+	const handleRestart = () => {
+		window.location.reload();
+	};
 	return (
 		<LoserMessageMessageContainer>
 			<LoserTitle>Du hast verloren!</LoserTitle>
+			<RestartButton onClick={handleRestart}>Neustart</RestartButton>
 		</LoserMessageMessageContainer>
 	);
 }
 
 const LoserMessageMessageContainer = styled.section`
-	background-color: #fff;
 	color: #000;
 	width: 100%;
 	height: 100%;
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	font-size: 36px;
@@ -21,4 +25,19 @@ const LoserMessageMessageContainer = styled.section`
 
 const LoserTitle = styled.h1`
 	color: red;
+`;
+
+const RestartButton = styled.button`
+	margin-top: 30px;
+	background-color: red;
+	color: white;
+	padding: 10px 20px;
+	font-size: 18px;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+
+	&:hover {
+		background-color: darkred;
+	}
 `;
