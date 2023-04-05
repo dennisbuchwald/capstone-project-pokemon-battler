cbfeccf524ebb00f4fead7b2b3c34700013353f9
Dennis Buchwald <buchwald95@icloud.com>
2023-04-05 10:11:37 +0200

    update enemystate

--- components/BattleScreen/BattleScreen.js
+++ components/BattleScreen/BattleScreen.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import React, { useState, useEffect } from "react";
 import { useBattleLogic } from "../../hooks/useBattleLogic";
 
 import PlayerPokemon from "./Pokemon/PlayerPokemon";
