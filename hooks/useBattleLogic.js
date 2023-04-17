9c18ca4f5ac611670c57612d900406c1affa35fb
Dennis Buchwald <buchwald95@icloud.com>
2023-04-17 10:12:34 +0200

    create an overview of what function does what to have a better overview of how the different difficulty levels are built in

--- hooks/useBattleLogic.js
+++ hooks/useBattleLogic.js
@@ -16,27 +16,12 @@ export function useBattleLogic(
 	const [enemyAttacking, setEnemyAttacking] = useState(false);
 	const [selectedEnemyIndex, setSelectedEnemyIndex] = useState(0);
 	const [potionCount, setPotionCount] = useState(3);
-
 	const [playSound] = useAttackedSound();
 	const [playHealthSound] = SoundEffect();
 
-	const handlePlayerDamage = (damage) => {
-		setPlayerHealth((prevHealth) => prevHealth - damage);
-	};
-
-	useEffect(() => {
-		if (playerAttacking) {
-			setTimeout(() => {
-				setPlayerAttacking(false);
-			}, 500);
-		}
-
-		if (enemyAttacking) {
-			setTimeout(() => {
-				setEnemyAttacking(false);
-			}, 500);
-		}
-	}, [playerAttacking, enemyAttacking]);
+	// ----------------------------------------------------------------
+	// Führt einen Angriff des Spielers aus, berechnet den Schaden,
+	// den der gegnerische Pokémon erleidet, und startet den Gegenangriff des Gegners.
 
 	const handleAttack = (attackIndex) => {
 		const attack = selectedPokemon.attacks[attackIndex];
@@ -45,6 +30,7 @@ export function useBattleLogic(
 		const actualDamage = Math.floor(
 			attack.damage * (Math.random() * 0.2 + 0.8)
 		);
+		console.log("Schaden am gegnerischen Pokémon:", actualDamage); // Schaden am gegnerischen Pokémon
 		const newCurrentHealth = Math.max(
 			selectedEnemyPokemon.currentHealth - actualDamage,
 			0
@@ -78,6 +64,7 @@ export function useBattleLogic(
 
 			setTimeout(() => {
 				const damageTaken = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+				console.log("Schaden am Spieler-Pokémon:", damageTaken); // Schaden am Spieler-Pokémon
 				handlePlayerDamage(damageTaken);
 
 				if (playerHealth - damageTaken <= 0) {
@@ -96,6 +83,31 @@ export function useBattleLogic(
 		}, 1000);
 	};
 
+	//  ----------------------------------------------------------------
+	//  Reduziert die Gesundheit des Spielers um den angegebenen Schadenswert
+
+	const handlePlayerDamage = (damage) => {
+		setPlayerHealth((prevHealth) => prevHealth - damage);
+	};
+
+	useEffect(() => {
+		if (playerAttacking) {
+			setTimeout(() => {
+				setPlayerAttacking(false);
+			}, 500);
+		}
+
+		if (enemyAttacking) {
+			setTimeout(() => {
+				setEnemyAttacking(false);
+			}, 500);
+		}
+	}, [playerAttacking, enemyAttacking]);
+
+	//----------------------------------------------------------------
+	// Aktualisiert den Status des Spiels, wenn ein gegnerisches Pokémon besiegt wird,
+	// und wechselt zum nächsten Pokémon in der Liste, falls vorhanden.
+
 	const handleEnemyDefeat = (enemyPokemons) => {
 		setIsEnemyDefeated(true);
 		setEnemyAttacking(false);
@@ -109,6 +121,10 @@ export function useBattleLogic(
 			return -1;
 		}
 	};
+
+	//----------------------------------------------------------------
+	//  Heilt den Spieler um den angegebenen Betrag, wenn er einen Heiltrank benutzt,
+	// und reduziert die Anzahl der verfügbaren Heiltränke.
 
 	const handlePotionUse = (healthToRestore) => {
 		if (potionCount <= 0) {
@@ -145,6 +161,8 @@ export function useBattleLogic(
 			}, 150);
 		}, 1000);
 	};
+
+	//----------------------------------------------------------------
 
 	return {
 		playerHealth,
