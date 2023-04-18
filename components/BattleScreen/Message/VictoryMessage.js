37aaef27da4909baa8baa88a150b74d9e618a0cc
Dennis Buchwald <buchwald95@icloud.com>
2023-04-18 16:52:09 +0200

    hotfix

--- components/BattleScreen/Message/VictoryMessage.js
+++ components/BattleScreen/Message/VictoryMessage.js
@@ -7,7 +7,7 @@ export default function VictoryMessage() {
 
 	return (
 		<VictoryMessageContainer>
-			<VictoryTitle>Du hast Gewonen</VictoryTitle>
+			<VictoryTitle>Du hast Gewonnen!</VictoryTitle>
 			<RestartButton onClick={handleRestart}>Neustart</RestartButton>
 		</VictoryMessageContainer>
 	);
