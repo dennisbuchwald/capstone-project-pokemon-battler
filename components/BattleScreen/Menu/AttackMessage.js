8d603c02a6f9ae80ac8326ccdcea6e0ce5fb6064
Dennis Buchwald <buchwald95@icloud.com>
2023-04-19 11:40:09 +0200

    css fix

--- components/BattleScreen/Menu/AttackMessage.js
+++ components/BattleScreen/Menu/AttackMessage.js
@@ -1,33 +1,33 @@
 import styled from "styled-components";
 
 const AttackMessage = ({ name, attack }) => {
-	return (
-		<AttackMessageBox>
-			<Message>
-				{name} setzt {attack} ein.
-			</Message>
-		</AttackMessageBox>
-	);
+  return (
+    <AttackMessageBox>
+      <Message>
+        {name} setzt {attack} ein.
+      </Message>
+    </AttackMessageBox>
+  );
 };
 
 export default AttackMessage;
 
 const AttackMessageBox = styled.section`
-	background-image: url("/sprites/text-box.png");
-	background-size: cover;
-	background-position: center;
+  background-image: url("/sprites/text-box.png");
+  background-size: cover;
+  background-position: center;
 
-	position: absolute;
-	width: 100%;
-	height: 79px;
-	left: 0%;
-	bottom: 0%;
-	z-index: 9999;
+  position: absolute;
+  width: 100%;
+  height: 79px;
+  left: 0%;
+  bottom: 0%;
+  z-index: 2;
 `;
 
 const Message = styled.p`
-	font-size: 20px;
-	line-height: 16px;
-	position: relative;
-	left: 4%;
+  font-size: 20px;
+  line-height: 16px;
+  position: relative;
+  left: 4%;
 `;
